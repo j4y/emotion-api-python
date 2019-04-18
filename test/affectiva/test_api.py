ada1fc944605a13be31c943209e38c08d94d8636
Jay Prall <jay.prall@affectiva.com>
2019-04-18 13:29:53 -0400

    Implement a way to specify a different index service url
    This is useful for developers to switch the EmotionAPI class to point
    at a local development environment or one of our test environments.
    
    Without this change, the init method makes a request to https://index.affectiva.com,
    which is the production server.

---
+++ test/affectiva/test_api.py
@@ -0,0 +1,48 @@
+import mock
+import os
+import unittest
+
+from affectiva.api import EmotionAPI
+
+
+class TestApi(unittest.TestCase):
+
+    def test_init_without_username(self):
+        with self.assertRaisesRegexp(ValueError, "No username provided."):
+            EmotionAPI(None, 'pass')()
+
+    def test_init_without_password(self):
+        with self.assertRaisesRegexp(ValueError, "No password provided."):
+            EmotionAPI('user', None)()
+
+    @mock.patch('requests.get')
+    def test_init_default_service_url(self, mockget):
+        mockresponse = mock.Mock()
+        mockget.return_value = mockresponse
+        expected_job_url = 'https://blah/jobs'
+        mockresponse.json.return_value = {"v1": {EmotionAPI.JOB_SERVICE_KEY: expected_job_url}}
+
+        expected_service_url = 'https://index.affectiva.com'
+
+        api = EmotionAPI('user', 'pass')
+        self.assertEqual(api._job_url, expected_job_url)
+        mockget.assert_called_once_with(expected_service_url, headers={'Accept': 'application/json'})
+
+    @mock.patch('requests.get')
+    def test_init_other_service_url(self, mockget):
+        mockresponse = mock.Mock()
+        mockget.return_value = mockresponse
+        expected_job_url = 'https://blah/jobs'
+        mockresponse.json.return_value = {"v1": {EmotionAPI.JOB_SERVICE_KEY: expected_job_url}}
+
+        expected_service_url = 'http://localhost:3001'
+        # set environment variable to overide service url
+        os.environ["AFFECTIVA_API_SERVICE_URL"] = expected_service_url
+
+        api = EmotionAPI('user', 'pass')
+        self.assertEqual(api._job_url, expected_job_url)
+        mockget.assert_called_once_with(expected_service_url, headers={'Accept': 'application/json'})
+
+
+if __name__ == '__main__':
+    unittest.main()
